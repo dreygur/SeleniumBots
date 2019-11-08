413abfcbb94cca191ec97430d80a578d6a839634
Rakibul Yeasin <rytotul@gmail.com>
2019-11-08 17:34:40 +0600

    Testing some codes

--- instagram.go
+++ instagram.go
@@ -5,8 +5,23 @@ package main
  * Instagram Version
  */
 
-// import "github.com/tebeka/selenium"
+import (
+	"fmt"
+
+	"github.com/tebeka/selenium"
+	"github.com/tebeka/selenium/firefox"
+)
 
 func main() {
-
+	caps := selenium.Capabilities{"browserName": "firefox"}
+	firecaps := firefox.Capabilities{
+		Args: []string{
+			"--headless",
+			"--no-sandbox",
+			"--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7",
+		},
+	}
+	caps.AddFirefox(firecaps)
+	wd, err := selenium.NewRemote(caps, "https://instagram.com")
+	fmt.Printf("%v\n%v\n", wd, err)
 }
